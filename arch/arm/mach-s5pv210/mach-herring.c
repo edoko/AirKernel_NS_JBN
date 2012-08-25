34e09462574545ebae03da67b307a05fcfa28902
edoko <r_data@naver.com>
2012-08-25 16:35:55 +0900

    mach-herring.c: Fix some - thanks to bedalus

--- arch/arm/mach-s5pv210/mach-herring.c
+++ arch/arm/mach-s5pv210/mach-herring.c
@@ -188,7 +188,7 @@ static void gps_gpio_init(void)
 
 static void uart_switch_init(void)
 {
-	int ret;
+	int ret = 0;
 	struct device *uartswitch_dev;
 
 	uartswitch_dev = device_create(sec_class, NULL, 0, NULL, "uart_switch");
@@ -407,11 +407,8 @@ static struct s3cfb_lcd r61408 = {
 					     (CONFIG_FB_S3C_NR_BUFFERS + \
 						 (CONFIG_FB_S3C_NUM_OVLY_WIN * \
 						  CONFIG_FB_S3C_NUM_BUF_OVLY_WIN)))
-//#ifdef CONFIG_S5PV210_HIGH_BIGMEM
+
 #define  S5PV210_VIDEO_SAMSUNG_MEMSIZE_JPEG (4092 * SZ_1K)
-//#else
-//#define  S5PV210_VIDEO_SAMSUNG_MEMSIZE_JPEG (8192 * SZ_1K)
-//#endif
 
 static struct s5p_media_device herring_media_devs[] = {
 	[0] = {
@@ -2239,7 +2236,7 @@ static void s5k4ecgx_init(void)
 static int s5k4ecgx_ldo_en(bool en)
 {
 	int err = 0;
-	int result;
+	int result = 0;
 
 	if (IS_ERR_OR_NULL(cam_isp_core_regulator) ||
 		IS_ERR_OR_NULL(cam_isp_host_regulator) ||
@@ -2546,7 +2543,7 @@ static int s5ka3dfx_power_init(void)
 static int s5ka3dfx_power_on(void)
 {
 	int err = 0;
-	int result;
+	int result = 0;
 
 	if (s5ka3dfx_power_init()) {
 		pr_err("Failed to get all regulator\n");
@@ -2941,7 +2938,7 @@ static struct attribute_group herring_properties_attr_group = {
 static void herring_virtual_keys_init(void)
 {
 	struct kobject *properties_kobj;
-	int ret;
+	int ret = 0;
 
 	properties_kobj = kobject_create_and_add("board_properties", NULL);
 	if (properties_kobj)
@@ -3054,7 +3051,7 @@ static void fsa9480_cardock_cb(bool attached)
 
 static void fsa9480_reset_cb(void)
 {
-	int ret;
+	int ret = 0;
 
 	/* for CarDock, DeskDock */
 	ret = switch_dev_register(&switch_dock);
