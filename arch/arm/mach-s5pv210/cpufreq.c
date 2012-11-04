bd76f74b4cfae407074cb8100dcdb5f483b5af5b
edoko <r_data@naver.com>
2012-11-04 15:59:41 +0900

    Revert "It is made sure that after wakeup from suspend for the next frequency"
    This reverts commit 70829345a16cdf28247f2fc8ddf7af3aaca05d79.

--- arch/arm/mach-s5pv210/cpufreq.c
+++ arch/arm/mach-s5pv210/cpufreq.c
@@ -178,7 +178,7 @@ static u32 clkdiv_val[8][11] = {
 #ifdef CONFIG_LIVE_OC
 extern void cpufreq_stats_reset(void);
 
-static bool pllbus_changing = false;
+static bool ocvalue_changed = false;
 
 static int oc_value = 100;
 
@@ -309,10 +309,10 @@ static int s5pv210_target(struct cpufreq_policy *policy,
 		bus_speed_changing = 1;
 
 #ifdef CONFIG_LIVE_OC
-	if (pllbus_changing) {
+	if (ocvalue_changed) {
 		pll_changing = 1;
 		bus_speed_changing = 1;
-		pllbus_changing = false;
+		ocvalue_changed = false;
 	}
 #endif
 
@@ -747,7 +747,7 @@ void liveoc_update(unsigned int oc_value)
     policy->user_policy.min = s5pv210_freq_table[index_min].frequency;
     policy->user_policy.max = s5pv210_freq_table[index_max].frequency;  
 
-    pllbus_changing = true;
+    ocvalue_changed = true;
 
     mutex_unlock(&set_freq_lock);
 
@@ -835,8 +835,6 @@ static int s5pv210_cpufreq_notifier_event(struct notifier_block *this,
 	case PM_POST_RESTORE:
 	case PM_POST_SUSPEND:
 #ifdef CONFIG_LIVE_OC
-		pllbus_changing = true;
-
 		cpufreq_driver_target(cpufreq_cpu_get(0), sleep_freq,
 				ENABLE_FURTHER_CPUFREQ);
 #else
